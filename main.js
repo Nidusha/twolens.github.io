637303320eab3e015becba44441eaafb40a77567
Nidusha <Nidushachanuja@gmail.com>
2025-12-24 22:37:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Counter Animation
+const counters = document.querySelectorAll('.number');
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 200;
+        if(count < target){
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target;
+        }
+    }
+    updateCount();
+});
